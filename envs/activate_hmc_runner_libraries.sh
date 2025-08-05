8da21e45425152ed02b6a5df514fa22b0198f23e
Fabio Delogu <fabio.delogu@cimafoundation.org>
2025-08-05 15:23:02 +0200

    chore(package) - update project after 1 year of inactivity

---
+++ envs/activate_hmc_runner_libraries.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Auto-generated script to activate the Conda environment 'hmc_runner_libraries'
+export PATH="../conda/bin:$PATH"
+source "../conda/bin/activate"
+conda activate "hmc_runner_libraries"
